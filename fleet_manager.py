63a27777328465aa0203edd80a63b79c4e829415
CS2071 <cs2071@kent.ac.uk>
2026-02-09 19:20:05 +0000

    Basic structure

---
+++ fleet_manager.py
@@ -0,0 +1,25 @@
+name = ["A", "B", "C"]
+rank = ["a", "b", "c"]
+division = ["1", "2", "3"]
+
+
+def main():
+    display_menu()
+
+def display_menu():
+    print("\n^ - Display Menu     -","\n1 - View Crew Files  -", "\n2 - Add Crew File    -", "\n3 - Update Crew File -", "\n4 - Remove Crew File -", "\n5 - Search Crew File -")
+    return input("Choose: ")
+
+# def crew_cut():
+    # View Crew
+
+    # Add Crew
+
+    # Update Crew
+
+    # Remove Crew
+
+    # Search Crew
+
+
+main()
