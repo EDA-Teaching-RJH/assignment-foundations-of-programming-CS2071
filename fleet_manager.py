9b3ac8377a409299b2f0fd5eaa43b4c023915c69
CS2071 <cs2071@kent.ac.uk>
2026-02-09 19:32:29 +0000

    attempting to tie together crew_cut and display_menu

--- fleet_manager.py
+++ fleet_manager.py
@@ -1,16 +1,25 @@
 name = ["A", "B", "C"]
 rank = ["a", "b", "c"]
 division = ["1", "2", "3"]
-
+dis = 0
 
 def main():
     display_menu()
+    crew_cut()
 
 def display_menu():
     print("\n^ - Display Menu     -","\n1 - View Crew Files  -", "\n2 - Add Crew File    -", "\n3 - Update Crew File -", "\n4 - Remove Crew File -", "\n5 - Search Crew File -")
-    return input("Choose: ")
+    dis = int(input("Choose: "))
 
-# def crew_cut():
+def crew_cut():
+    if dis == 0:
+        print("it broke")
+    if dis == 1:
+        for a in range(lens(name)):
+            print(name, " - ", rank, " - ", division, " - ", name.index())
+            a = a +1
+        print("if works")
+
     # View Crew
 
     # Add Crew
