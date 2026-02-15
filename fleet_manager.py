2ff2ae2649cb9ee4b0baf059dc72c9e56fe351f8
CS2071 <cs2071@kent.ac.uk>
2026-02-15 18:28:47 +0000

    Finished Name Remove, also added extra stuff to make things look nicer

--- fleet_manager.py
+++ fleet_manager.py
@@ -9,14 +9,18 @@ def fleet_manager():
     while True:
 
         print("\n^ - Display Menu     -","\n1 - View Crew Files  -", "\n2 - Add Crew File    -", "\n3 - Update Crew File -", "\n4 - Remove Crew File -", "\n5 - Search Crew File -", "\n6 - Leave            -")
+        # Funny
         try:
             dis = int(input("^ - Choose: "))
 
             # View Crew
             if dis == 1:
+                print("*----------------")
+                print("\n| Name", " - ", "Rank", " - ", "Division", " - ", "ID")
                 for a in range(len(name)):
-                    print(name[a], " - ", rank[a], " - ", division[a], " - ", a+1)
+                    print("| ", name[a], " - ", rank[a], " - ", division[a], " - ", a+1)
                     a = a +1
+                print("*----------------")
         
             # Add Crew
             elif dis == 2:
@@ -45,7 +49,7 @@ def fleet_manager():
             # Remove Crew
             elif dis == 4:
                 Rask = input("\n* Index (I) or Names (N)? : ")
-                if Rask == "I":
+                if Rask == "I" or Rask == "i":
                     print("| IDs from 1 to", len(name))
                     try:
                         IDRemove = int(input("| Which ID do you want to remove? : ")) -1
@@ -60,8 +64,19 @@ def fleet_manager():
                         print("| Invalid input...", "\n| Try again...")
 
 
-                if Rask == "N":
-                    print(name)
+                if Rask == "N" or Rask == "n":
+                    print("|", name)
+                    try:
+                        NameRemove = name.index(str(input("| Which Name do you want to remove? :")))
+                        name.pop(int(NameRemove))
+                        print("| Name has been removed...")
+                        rank.pop(int(NameRemove))
+                        print("| Rank has been removed...")
+                        division.pop(int(NameRemove))
+                        print("| Divison has been removed...")
+                        print("*----------------", "\n| Crew file has been cleared.")
+                    except ValueError:
+                        print("| Invalid input...", "\n| Try again...")
 
             # Search Crew
             elif dis == 5:
